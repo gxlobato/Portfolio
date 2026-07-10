33c09389e1c99f8e841ce1088bc1bf1d97084ebf
gxlobato <106767767+gxlobato@users.noreply.github.com>
2026-07-10 13:44:49 -0300

    Delete estoque_farmaceutico/set_git_user.py

--- estoque_farmaceutico/set_git_user.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-nome = input("Seu nome: ").strip()
-email = input("Seu email do GitHub: ").strip()
-
-subprocess.run(f'git config --global user.name "{nome}"', shell=True)
-subprocess.run(f'git config --global user.email "{email}"', shell=True)
-
-print("Pronto. Configuração global atualizada.")
